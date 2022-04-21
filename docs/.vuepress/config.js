9dcca83038258550be70d9207bbc03b9ca9123e7
rck <296046734@qq.com>
2022-04-21 13:27:58 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: "TCP",
+    base: '/tcp/',
+    themeConfig: {
+      nav: [{
+        text: 'Home', link: '/'
+      }, {
+        text: 'Github', link: 'https://github.com/l1872427108/uui'
+      }, {
+        text: 'Npm', link: 'https://www.npmjs.com/package/uu-uui'
+      }],
+      sidebar: [
+        'componentDocs/TCP和UDP',
+        'componentDocs/TCP三次握手',
+        'componentDocs/TCP四次挥手',
+        'componentDocs/SYN Flood攻击',
+        'componentDocs/半连接队列',
+        'componentDocs/TCP快速打开原理',
+        'componentDocs/TCP报文头部字段',
+        'componentDocs/TCP报文中时间戳',
+        'componentDocs/TCP的超时重传时间',
+        'componentDocs/TCP流量控制'
+      ]
+    }
+  }
+  
+  
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+# Hello VuePress
+
+### 特点
+1. 在客户端发送 ajax 请求
+2. 在 node 端中发送 http 请求。
+3. 支持 promise
+4. 拦截器
+5. 取消请求
+6. 转为 json 格式数据
+7. 可以阻止一些跨站攻击 xsrf。 做到保护作用。
